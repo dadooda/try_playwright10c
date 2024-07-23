a1e5d0a99596cc742654b8f8adcfbaed2befad0b
Alex Fortuna <fortunadze@gmail.com>
2024-07-23 17:25:55 +0300

    [skip ci] Moving on

--- pw1/try1/lib/env.ts
+++ pw1/try1/lib/env.ts
@@ -1,19 +1,25 @@
 
 /**
- * Осмысленно работаем с environment-установками.
+ * Инструменты для осмысленной работы с env-переменными.
  * @module
  */
 
 import 'dotenv/config';
 
 /**
- * Читаем значение  и возвращаем его.
+ * Читаем значение и возвращаем его.
+ */
+export function get(key: string): string | undefined {
+  return process.env[key];
+}
+
+/**
+ * Читаем значение и возвращаем его.
  * Если ключ не найден, пуляем исключение.
  */
 export function require(key: string): string | never {
   let v: string | undefined;
-  const m = console.log;
-  if ((v = process.env[key]) == undefined) throw new Error(`Key not found: ${_(key)}`)
+  if ((v = get(key)) == undefined) throw new Error(`Env variable is not set: ${_(key)}`)
   return v
 }
 
